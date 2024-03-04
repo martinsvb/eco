15c103e26900114d9cfe774d61c3a2535858fa43
Martin <svobodamartin@centrum.cz>
2024-03-04 16:41:22 +0100

    Vite config updated

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,9 @@
 /// <reference types='vitest' />
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
-export default ({ mode }: any) => defineConfig({
+export default () => defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/web',
 
